2a6c41a34ed0e8d4e62498baa9354f27267754a1
sabarthazo <sabarthazo@gmail.com>
2022-11-13 16:07:47 +0100

    Squelette Sous-Classe Pion

---
+++ app/src/main/java/javaproject/utt/a22/Maitre.java
@@ -0,0 +1,20 @@
+package javaproject.utt.a22;
+
+public class Maitre extends Pion{
+    
+    /**
+     * Main
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+
+
+    /**
+     * Constructeur de la classe Maitre.
+     */
+    public Maitre(){
+
+    }
+}
