0c01909cdca5809f06f2ba2099e6737f7af027ac
sabarthazo <sabarthazo@gmail.com>
2022-11-29 15:50:04 +0100

    Debut implementation de zone

--- app/src/main/java/javaproject/utt/a22/Zone.java
+++ app/src/main/java/javaproject/utt/a22/Zone.java
@@ -10,14 +10,19 @@ public class Zone{
     Plateau plateau;
 
     /**
-     * Set de pion de la team 1 present sur la zone.
+     * List de pion de la team 1 present sur la zone.
      */
     LinkedList<Pion> linkedPionTeam1 = new LinkedList<Pion>();
 
     /**
-     * Set de pion de la team 2 present sur la zone.
+     * List de pion de la team 2 present sur la zone.
      */
     LinkedList<Pion> linkedPionTeam2 = new LinkedList<Pion>();
+
+    /**
+     * List de pion de la zone.
+     */
+    LinkedList<Pion> linkedPion = new LinkedList<Pion>();
 
     /**
      * Attribut correspondant au nom de la zone.
@@ -48,7 +53,37 @@ public class Zone{
      * @param args
      */
     public static void main(String[] args) {
-        
+        Partie partie = new Partie();
+
+        Joueur joueur = new Joueur(partie, "Joueur 1");
+
+        Zone zone = new Zone();
+
+        Pion pion1 = new Etudiant(joueur, "Etudiant 1");
+        Pion pion2 = new Etudiant(joueur, "Etudiant 2");
+        Pion pion3 = new Etudiant(joueur, "Etudiant 3");
+        Pion pion4 = new Etudiant(joueur, "Etudiant 4");
+
+        zone.addPion(pion1);
+        zone.addPion(pion2);
+        zone.addPion(pion3);
+        zone.addPion(pion4);
+
+        Iterator<Pion> itBefore = zone.linkedPion.iterator();
+        while(itBefore.hasNext()){
+            System.out.println(itBefore.next());
+        }
+
+        pion1.changerInitiative(3);
+        pion2.changerInitiative(8);
+        pion3.changerInitiative(7);
+
+        zone.sortLinkedPion();
+
+        Iterator<Pion> itAfter = zone.linkedPion.iterator();
+        while(itAfter.hasNext()){
+            System.out.println(itAfter.next());
+        }
     }
 
 
@@ -65,6 +100,9 @@ public class Zone{
      * @param pion
      */
     public void addPion(Pion pion){
+        if(!this.linkedPion.contains(pion)){
+            this.linkedPion.add(pion);
+        }
         /*
          * Ajouter le pion au set de pion de la team 1 de la zone.
          */
@@ -149,9 +187,67 @@ public class Zone{
     /**
      * Methode pour recuperer le status de la zone.
      * @return this.status
+     * FONCTIONNEL
      */
     public StatusZone getStatus(){
         return this.status;
+    }
+
+
+    /**
+     * Methode pour trier la liste de la team 1 par ECTS
+     * FONCTIONNEL MAIS INVERSE
+     */
+    public void sortLinkedPionTeam1(){
+        Collections.sort(linkedPionTeam1, new Comparator<Pion>(){
+            @Override
+            public int compare(Pion p1, Pion p2){
+                return p2.ECTS - p1.ECTS;
+            }
+        });
+    }
+
+    /**
+     * Methode pour trier la liste de la team 2 par ECTS
+     * FONCTIONNEL MAIS INVERSE
+     */
+    public void sortLinkedPionTeam2(){
+        Collections.sort(linkedPionTeam2, new Comparator<Pion>(){
+            @Override
+            public int compare(Pion p1, Pion p2){
+                return p2.ECTS - p1.ECTS;
+            }
+        });
+    }
+
+    /**
+     * Methode pour trier la liste de la zone par initiative
+     * FONCTIONNEL
+     */
+    public void sortLinkedPion(){
+        Collections.sort(linkedPion, new Comparator<Pion>(){
+            @Override
+            public int compare(Pion p1, Pion p2){
+                return p2.initiative - p1.initiative;
+            }
+        });
+    }
+
+
+    /**
+     * Methode pour lancer le combat de la zone.
+     */
+    public void combat(){
+        this.sortLinkedPion();
+        this.sortLinkedPionTeam1();
+        this.sortLinkedPionTeam2();
+
+        Iterator<Pion> itInitiative = this.linkedPion.iterator();
+        while(itInitiative.hasNext()){
+            Pion pionInitiative = itInitiative.next();
+            Pion pionECTS = linkedPionTeam1.getLast();
+            pionInitiative.executerStrategie(pionECTS);
+        }
     }
 
 }
