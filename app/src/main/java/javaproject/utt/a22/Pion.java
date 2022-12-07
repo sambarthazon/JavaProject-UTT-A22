b0ae267f5159af20363412aab8ed16ce76019c26
sabarthazo <sabarthazo@gmail.com>
2022-12-07 17:56:54 +0100

    Implementation des methodes de Pion et commentaires

--- app/src/main/java/javaproject/utt/a22/Pion.java
+++ app/src/main/java/javaproject/utt/a22/Pion.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Pion{
 
     /**
-     * Attribut specifiant à quel joueur appartient le pion.
+     * Attribut indiquant a quel joueur appartient le pion.
      */
     protected Joueur joueur;
 
@@ -18,15 +18,15 @@ public class Pion{
     protected String nom;
 
     /**
-     * L'attribut ECTS équivaut à la vie du pion, une fois à 0 notre pion meurt.
+     * L'attribut ECTS equivaut à la vie du pion, une fois à 0 notre pion meurt.
      */
     protected int ECTS = this.ECTSDefault;
     /**
-     * 
+     * ECTS de base du pion.
      */
     protected final int ECTSDefault = 30;
     /**
-     * 
+     * ECTS maximum que le pion peut avoir.
      */
     protected int ECTSMax = this.ECTSDefault + this.constitution;
 
@@ -101,10 +101,9 @@ public class Pion{
     private StatusPion status = StatusPion.Indefini;
 
     /**
-     * Strategie que le pion adaptera pour combattre (Offensif, Defensif ou Aleatoire).
+     * Strategie que le pion adaptera pour combattre (Offensif, Defensif, Aleatoire ou Preferentielle).
      */
     protected Strategie strategie;
-
 
     /**
      * Zone a laquelle le pion est affecte.
@@ -117,16 +116,16 @@ public class Pion{
      * @param args
      */
     public static void main(String[] args){
-        Partie partie = new Partie();
-
-        Joueur joueur = new Joueur(partie, "Joueur 1");
-
-        for(int i = 0; i<20; i++){
-            Pion etudiant = new Etudiant(joueur, "Etudiant "+i);
-            joueur.addPion(etudiant);
-        }
+        
     }
 
+
+
+    //******************************************************//
+    //                                                      //
+    //                      Constructeur                    //
+    //                                                      //
+    //******************************************************//
 
     /**
      * Constructeur Pion
@@ -135,8 +134,49 @@ public class Pion{
 
     }
 
+
+
+    //******************************************************//
+    //                                                      //
+    //                        Joueur                        //
+    //                                                      //
+    //******************************************************//
+
+    /**
+     * Methode pour recuprer le joueur du pion.
+     * @return this.joueur
+     */
+    public Joueur getJoueur(){
+        return this.joueur;
+    }
+
+
+
+    //******************************************************//
+    //                                                      //
+    //                          Nom                         //
+    //                                                      //
+    //******************************************************//
+
+    /**
+     * Methode pour recuperer le nom du pion.
+     * @return this.nom
+     */
+    public String getNom(){
+        return this.nom;
+    }
+
+
+
+    //******************************************************//
+    //                                                      //
+    //                          ECTS                        //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour ajouter ou retirer des ECTS au pion.
+     * Les ECTS ne pourront pas etre au dessus des ECTS maximum du pion.
      * @param ECTS
      */
     public void setECTS(int ECTS){
@@ -149,17 +189,23 @@ public class Pion{
     /**
      * Methode pour recuperer les ECTS du pion.
      * @return this.ECTS
-     * FONCTIONNEL
      */
     public int getECTS(){
         return this.ECTS;
     }
 
 
+
+    //******************************************************//
+    //                                                      //
+    //                        Dexterite                     //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour changer la dexterite du pion.
+     * Le dexterite est bornee par maxDexterite et minDexterite.
      * @param dexterite
-     * FONCTIONNEL
      */
     public void setDexterite(int dexterite){
         if(this.joueur.getListPion().contains(this)){ //Verification que ce soit le pion du joueur.
@@ -182,17 +228,23 @@ public class Pion{
     /**
      * Methode pour recuperer la dexterite du pion.
      * @return this.dexterite
-     * FONCTIONNEL
      */
     public int getDexterite(){
         return this.dexterite;
     }
 
 
+
+    //******************************************************//
+    //                                                      //
+    //                          Force                       //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour changer la force du pion.
+     * La force est bornee par maxForce et minForce.
      * @param force
-     * FONCTIONNEL
      */
     public void setForce(int force){
         if(this.joueur.getListPion().contains(this)){ //Verification que ce soit le pion du joueur.
@@ -215,17 +267,23 @@ public class Pion{
     /**
      * Methode pour recuperer la force du pion.
      * @return this.force
-     * FONCTIONNEL
      */
     public int getForce(){
         return this.force;
     }
 
 
+
+    //******************************************************//
+    //                                                      //
+    //                      Resistance                      //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour changer la resistance du pion.
+     * La resistance est bornee par maxResistance et minResistance.
      * @param resistance
-     * FONCTIONNEL
      */
     public void setResistance(int resistance){
         if(this.joueur.getListPion().contains(this)){ //Verification que ce soit le pion du joueur.
@@ -248,17 +306,24 @@ public class Pion{
     /**
      * Methode pour recuperer la resistance du pion.
      * @return this.resistance
-     * FONCTIONNEL
      */
     public int getResistance(){
         return this.resistance;
     }
 
 
+
+    //******************************************************//
+    //                                                      //
+    //                      Constitution                    //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour changer la constitution du pion.
+     * La constitution est bornee par maxConsitution et minConstitution.
+     * Les ECTS max vont evolue en fonction de l'evolution de la consitution.
      * @param constitution
-     * FONCTIONNEL
      */
     public void setConstitution(int constitution){
         if(this.joueur.getListPion().contains(this)){ //Verification que ce soit le pion du joueur.
@@ -292,17 +357,22 @@ public class Pion{
     /**
      * Methode pour recuperer la constitution du pion.
      * @return this.constitution
-     * FONCTIONNEL
      */
     public int getConstitution(){
         return this.constitution;
     }
 
 
+
+    //******************************************************//
+    //                                                      //
+    //                      Initiative                      //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour changer l'initiative du pion.
      * @param initiative
-     * FONCTIONNEL
      */
     public void setInitiative(int initiative){
         if(this.joueur.getListPion().contains(this)){ //Verification que ce soit le pion du joueur.
@@ -325,21 +395,26 @@ public class Pion{
     /**
      * Methode pour recuperer l'initiative du pion.
      * @return this.initiative
-     * FONCTIONNEL
      */
     public int getInitiative(){
         return this.initiative;
     }
 
 
+
+    //******************************************************//
+    //                                                      //
+    //                         Status                       //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour changer le status de combat du pion.
      * Verification du status souhaite puis verification du nombre de pion sous ce status.
      * @param status
-     * FONCTIONNEL
      */
     public void setStatus(StatusPion status){
-        if(status == StatusPion.Combattant){
+        if(status.equals(StatusPion.Combattant)){
             int var = 0;
             Iterator<Pion> it = this.joueur.getListPion().iterator();
             while(it.hasNext()){
@@ -353,7 +428,7 @@ public class Pion{
             } else{
                 this.status = StatusPion.Combattant;
             }
-        } else if(status == StatusPion.Reserviste){
+        } else if(status.equals(StatusPion.Reserviste)){
             this.status = StatusPion.Reserviste;
         } else{
             System.out.println("Votre pion ne peut que être un Combattant ou un Reserviste.");
@@ -363,16 +438,23 @@ public class Pion{
     /**
      * Methode pour recuperer le status de combat du pion.
      * @return this.status
-     * FONCTIONNEL
      */
     public StatusPion getStatus(){
         return this.status;
     }
 
 
+
+    //******************************************************//
+    //                                                      //
+    //                          Zone                        //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour changer la zone du pion.
-     * Verification si nous changeons avec la meme zone. Si non, enlever le pion de la zone et mettre la nouvelle zone au pion.
+     * Verification si nous changeons avec la meme zone.
+     * Sinon, enlever le pion de la zone et mettre la nouvelle zone au pion.
      * @param zone
      */
     public void setZone(Zone zone){
@@ -392,17 +474,22 @@ public class Pion{
     /**
      * Methode pour recuperer la zone du pion.
      * @return
-     * FONCTIONNEL
      */
     public Zone getZone(){
         return this.zone;
     }
 
 
+
+    //******************************************************//
+    //                                                      //
+    //                        Strategie                     //
+    //                                                      //
+    //******************************************************//
+
     /**
      * Methode pour changer la strategie du pion.
      * @param strategie
-     * FONCTIONNEL
      */
     public void setStrategie(Strategie strategie){
         this.strategie = strategie;
@@ -410,8 +497,7 @@ public class Pion{
 
     /**
      * Methode pour recuperer la strategie du pion.
-     * @return
-     * FONCTIONNEL
+     * @return this.strategie.getNom();
      */
     public String getStrategie(){
         return this.strategie.getNom();
@@ -419,46 +505,22 @@ public class Pion{
 
     /**
      * Methode pour combattre en fonction de la strategie attribuee.
-     * FONCTIONNEL
      */
     public void executerStrategie(){
         this.strategie.combattre(this);
     }
 
 
-    /**
-     * Methode pour parametrer un joueur au pion.
-     * Verification si nous settons avec le meme joueur. Si non, enelever le pion du joueur et mettre le nouveau joueur au pion.
-     * @param joueur
-     * FONCTIONNEL
-     */
-    public void setJoueur(Joueur joueur){
-        if(this.joueur.equals(joueur)){
-            System.out.println("Ce pion appartient déjà à ce joueur.");
-        } else{
-            this.joueur.removePion(this);
-            this.joueur = null;
-        }
 
-        if(this.joueur == null){
-            this.joueur = joueur;
-            this.joueur.addPion(this);
-        }
-    }
+    //******************************************************//
+    //                                                      //
+    //                        Affichage                     //
+    //                                                      //
+    //******************************************************//
 
     /**
-     * Methode pour recuprer le joueur du pion.
-     * @return this.joueur
-     */
-    public Joueur getJoueur(){
-        return this.joueur;
-    }
-
-
-    /**
-     * Redefinition de la methode toString
+     * Redefinition de la methode toString.
      * @return
-     * FONCTIONNEL
      */
     @Override
     public String toString(){
